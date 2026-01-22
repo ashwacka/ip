c66b20d5e509487ccd5357d7a7251dfeae4bf216
ashwacka <ashwikagupta@gmail.com>
2026-01-22 16:23:31 +0800

    Level 1. Echo

--- src/main/java/Wacka.java
+++ src/main/java/Wacka.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Wacka {
     public static void main(String[] args) {
-        String divider = "--------------------------";
-        String intro = "Wacka";
-        String greeting = "What can I do for you?";
-        System.out.println(divider + '\n' + "Hello, I'm " + intro + '\n' + greeting + '\n' + divider);
-        String outro = "Bye. Hope to see you soon.";
-        System.out.println(outro);
+        String divider = "______________________________________";
+        System.out.println(divider);
+        System.out.println("Hello I'm Wacka!");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(divider);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                break;
+            } else {
+                System.out.println(divider);
+                System.out.println(input);
+                System.out.println(divider);
+            }
+        }
+        scanner.close();
     }
 }
