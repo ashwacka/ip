f21d63edc34ec119b132f06c7fe40eb698e7b2c3
ashwacka <ashwikagupta@gmail.com>
2026-01-22 17:48:27 +0800

    Level 2. Add, List

--- src/main/java/Wacka.java
+++ src/main/java/Wacka.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Wacka {
     public static void main(String[] args) {
@@ -9,18 +10,31 @@ public class Wacka {
         System.out.println(divider);
 
         Scanner scanner = new Scanner(System.in);
+        String[] arr = new String[100];
+        int i = 0;
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
+            //base case, terminating factor
             if (input.equals("bye")) {
                 System.out.println(divider);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(divider);
                 break;
-            } else {
+            }
+            if (!input.equals("list")) {
+                arr[i] = input;
+                i++;
                 System.out.println(divider);
-                System.out.println(input);
+                System.out.println("added: " + input);
+                System.out.println(divider);
+            } else if (input.equals("list")) {
+                System.out.println(divider);
+                for (int j = 0; j < i; j++) {
+                    System.out.println(j + 1 + ". " + arr[j]);
+                }
                 System.out.println(divider);
             }
+
         }
         scanner.close();
     }
